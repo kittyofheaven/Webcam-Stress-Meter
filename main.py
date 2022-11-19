4f59f39b631db73ce3b21c40c4e541aace7bc38e
Hazel Handrata <71081063+kittyofheaven@users.noreply.github.com>
2022-11-19 23:29:58 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+import sys
+
+# webcam choose
+webcam = None
+if len(sys.argv) == 2:
+    webcam = cv2.VideoCapture(sys.argv[1])
+else:
+    webcam = cv2.VideoCapture(0)
